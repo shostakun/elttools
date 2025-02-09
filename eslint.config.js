53555e7a350fc9fef8343e1ab6fc530f73237b7b
shostakun <23522495+shostakun@users.noreply.github.com>
2025-02-09 16:30:25 +0900

    Set up eslint and prettier.

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+
+/** @type {import('eslint').Linter.Config[]} */
+const config = [
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  { ignores: ["dist/*"] },
+];
+
+export default config;
--- src/components/ToolList.vue
+++ src/components/ToolList.vue
@@ -1,7 +1,11 @@
 <template>
   <h1>ELT Tools</h1>
   <h2>for the classroom or online</h2>
-  <p>These are tools I've built for my own use over the years. Many/most of them have been designed with online lessons in mind, but I use them in classroom settings as well. Each has some info about how I use them.</p>
+  <p>
+    These are tools I've built for my own use over the years. Many/most of them
+    have been designed with online lessons in mind, but I use them in classroom
+    settings as well. Each has some info about how I use them.
+  </p>
   <ul>
     <li>
       <RouterLink to="/color">Random Color</RouterLink>
