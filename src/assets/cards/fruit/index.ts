3e5edb4326adf78a5b5e6d963e2e76b394d39f04
shostakun <23522495+shostakun@users.noreply.github.com>
2025-03-10 14:56:06 +0900

    Added more fruit.

--- src/assets/cards/fruit/index.ts
+++ src/assets/cards/fruit/index.ts
@@ -1,8 +1,13 @@
 // [[[cog from gen_cards import *; files = get_card_files(); gen_card_imports(files)]]]
 import appleCard from "./apple.png";
 import bananaCard from "./banana.png";
+import canteloupeCard from "./canteloupe.png";
 import kiwiCard from "./kiwi.png";
 import orangeCard from "./orange.png";
+import peachCard from "./peach.png";
+import pearCard from "./pear.png";
+import pineappleCard from "./pineapple.png";
+import strawberryCard from "./strawberry.png";
 import watermelonCard from "./watermelon.png";
 // [[[end]]]
 import { type CardSet } from "@/types/cards";
@@ -18,6 +23,10 @@ const imageMap = {
     images: [bananaCard],
     tags: ["anime", "banana", "fruit", "one"],
   },
+  canteloupe: {
+    images: [canteloupeCard],
+    tags: ["anime", "canteloupe", "fruit", "one"],
+  },
   kiwi: {
     images: [kiwiCard],
     tags: ["anime", "fruit", "kiwi", "one"],
@@ -25,6 +34,22 @@ const imageMap = {
   orange: {
     images: [orangeCard],
     tags: ["anime", "fruit", "one", "orange"],
+  },
+  peach: {
+    images: [peachCard],
+    tags: ["anime", "fruit", "one", "peach"],
+  },
+  pear: {
+    images: [pearCard],
+    tags: ["anime", "fruit", "one", "pear"],
+  },
+  pineapple: {
+    images: [pineappleCard],
+    tags: ["anime", "fruit", "one", "pineapple"],
+  },
+  strawberry: {
+    images: [strawberryCard],
+    tags: ["anime", "fruit", "one", "strawberry"],
   },
   watermelon: {
     images: [watermelonCard],
