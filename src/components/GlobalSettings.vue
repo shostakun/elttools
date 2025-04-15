6c795462672864d9ef73ab680d10cd7d7a3c43a0
shostakun <23522495+shostakun@users.noreply.github.com>
2025-04-15 12:42:13 +0900

    Added font switcher.

--- src/components/GlobalSettings.vue
+++ src/components/GlobalSettings.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
 import { useDarkMode } from "@/utils/darkMode";
+import { onMounted, ref, watch } from "vue";
 
 const { darkMode, storeDarkMode } = useDarkMode();
 
 const handleToggle = () => {
   storeDarkMode();
 };
+
+const fontOptions = [
+  { title: "Atkinson", value: '"Atkinson Hyperlegible Mono"' },
+  { title: "Alata" },
+  { title: "OpenDyslexic" },
+].map((obj) => ({ value: `"${obj.title}"`, ...obj }));
+
+const selectedFont = ref("Alata");
+
+watch(selectedFont, () =>
+  document.documentElement.style.setProperty(
+    "--v-font-family",
+    selectedFont.value,
+  ),
+);
+
+onMounted(() => {
+  const rootStyles = getComputedStyle(document.documentElement);
+  const font = rootStyles.getPropertyValue("--v-font-family").trim();
+  if (fontOptions.find(({ value }) => value === font)) {
+    selectedFont.value = font;
+  }
+});
 </script>
 
 <template>
@@ -14,4 +38,11 @@ const handleToggle = () => {
     label="Dark mode"
     @update:model-value="handleToggle"
   />
+  <v-select v-model="selectedFont" :items="fontOptions" label="Font"
+    ><template #item="{ props: itemProps, item }">
+      <v-list-item
+        v-bind="itemProps"
+        :style="{ fontFamily: item.value }"
+      ></v-list-item> </template
+  ></v-select>
 </template>
